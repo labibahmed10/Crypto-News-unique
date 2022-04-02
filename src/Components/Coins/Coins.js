5ee24ecd8289aecd5a7be8f0e014076436a33d84
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-02 16:47:44 +0600

    spinner added

--- src/Components/Coins/Coins.js
+++ src/Components/Coins/Coins.js
@@ -1,30 +1,40 @@
 import React, { useEffect, useState } from "react";
 import CoinCard from "../CoinCard/CoinCard";
+import Spinner from "../Spinner/Spinner2";
 
 const Coins = () => {
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState([]);
 
   useEffect(() => {
+    setLoading(false);
     fetch(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
     )
       .then((res) => res.json())
       .then((data) => setCoins(data));
+    setLoading(true);
   }, []);
 
   return (
-    <section>
-      <h1 className="text-center font-semibold md:text-4xl text-xl font-mono mt-5">
-        Top 50 Crypto Currencies
-      </h1>
-      <p className="text-center py-3 md:text-2xl">Available Now: {coins.length} </p>
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <section>
+          <h1 className="text-center font-semibold md:text-4xl text-xl font-mono mt-5">
+            Top 50 Crypto Currencies
+          </h1>
+          <p className="text-center py-3 md:text-2xl">Available Now: {coins.length} </p>
 
-      <section className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 my-5">
-        {coins.map((coin) => (
-          <CoinCard key={coin.id} coin={coin}></CoinCard>
-        ))}
-      </section>
-    </section>
+          <section className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 my-5">
+            {coins.map((coin) => (
+              <CoinCard key={coin.id} coin={coin}></CoinCard>
+            ))}
+          </section>
+        </section>
+      )}
+    </>
   );
 };
 
