f545dbbeb71c9346793d359350c14785afa6c229
labibahmed10 <labib.ahmed.372@gmail.com>
2022-10-29 15:43:27 +0600

    basic overview is done

--- cypress/e2e/prac.cy.js
+++ cypress/e2e/prac.cy.js
@@ -2,12 +2,17 @@
 
 describe("Trying to figure out what cypress is", () => {
    it("should go to the signup page and sign up a user", () => {
-      cy.visit("https://extraordinary-zabaione-d385bf.netlify.app/signup");
+      // cy.visit("http://localhost:3000/login");
+      // cy.get(".underline").click();
+      // cy.location("pathname").should("eql", "/signup");
 
-      cy.get("#email").type("labib.372@gmail.com");
-      cy.get("input[name='password']").type("asdasd");
-      cy.get("input[name='conpassword']").type("asdasd");
-      cy.get("#checkbox").click();
-      cy.get("input[name='submit']");
+      cy.visit("https://extraordinary-zabaione-d385bf.netlify.app");
+
+      // cy.get("#email").type("labib.372@gmail.com");
+      // cy.get("input[name='password']").type("asdasd");
+      // cy.get("input[name='conpassword']").type("asdasd");
+      // cy.get("#checkbox").click().should("be.enabled");
+      // cy.get("input[name='submitbtn']");
+      // cy.contains("Signup");
    });
 });
